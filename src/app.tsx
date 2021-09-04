3b25798f7b16979b348fa5f2358a943a7855f6d0
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-09-04 18:09:46 +0900

    Appからappに修正(pushすると何故か勝手に小文字に変換されるため)

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,9 @@ import { BrowserRouter } from "react-router-dom";
 import PageRouter from "./components/pages/router/PageRouter";
 import "./styles.css";
 
-const App = () => (
+export const App = () => (
   <BrowserRouter>
     <PageRouter />
   </BrowserRouter>
 );
 
-export default App;
